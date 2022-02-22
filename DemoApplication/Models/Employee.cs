23d050e0181edc4194ab2f269583309d7fe92c5b
DESKTOP-P6BU8DQ\hp <sonal.patel@archesoftronix.com>
2022-02-22 13:01:11 +0530

    Add project files.

---
+++ DemoApplication/Models/Employee.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DemoApplication.Models
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public DateTime DOB { get; set; }
+        public int Salary { get; set; }
+        public string City { get; set; }
+    }
+}
